c1d6132d0a3341a49a1a21e244cab02d6b3dd2c5
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-07 09:55:35 -0300

    ADD animação de escala no penguin

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imageView: UIImageView!
+    var currentAnimation = 0
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+    }
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: []) {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                
+            default:
+                break
+            }
+        } completion: { finished in
+            sender.isHidden = false
+        }
+
+        currentAnimation = (currentAnimation + 1) % 8 // Ao chegar no 8 volta pra 0, pois 7 animações
     }
 
 
